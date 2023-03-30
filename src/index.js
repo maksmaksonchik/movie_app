e4e99ca48b710a7679462c454e8ed25ceb9fed2b
Maksim Efremov <efremovmax2002@gmail.com>
2023-03-30 21:05:53 +0500

    feat: add movie rendering (bad)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
+import { mapMovie } from './helpers/mapMovie.js';
+
 // Components
 import './components/currentYear.js';
 
-const search = document.querySelector('.main');
+const movieTemplate = document.querySelector('#movie');
 
+const render = (movieData) => {
+  const movie = movieTemplate.content.cloneNode(true);
+
+  const link = movie.querySelector('.movie');
+  const title = movie.querySelector('.movie__name');
+  const poster = movie.querySelector('.movie__poster');
+  const year = movie.querySelector('.movie__year');
+  const genre = movie.querySelector('.movie__genres');
+  const rating = movie.querySelector('.movie__rating');
+
+  poster.src = movieData.poster;
+  title.textContent = movieData.title;
+  year.textContent = movieData.year;
+  link.href = movieData.link;
+
+  return movie;
+};
+
+const resultsContainer = document.querySelector('.results__grid');
+
+const search = document.querySelector('.main');
 const activateSearch = () => {
   search.classList.add('search_active');
 };
 
 activateSearch();
+
+const main = async () => {
+  const { Search } = await fetch('./src/data.json').then((r) => r.json());
+  const movies = Search.map((result) => render(mapMovie(result)));
+
+  const fragment = document.createDocumentFragment();
+
+  movies.forEach((movie) => fragment.appendChild(movie));
+  resultsContainer.appendChild(fragment);
+};
+
+main();
